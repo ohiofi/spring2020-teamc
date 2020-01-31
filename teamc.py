db590a8f5fe0e6bdfd39aca0627859f9e9bf7e90
calliejh <57910979+calliejh@users.noreply.github.com>
2020-01-31 11:57:23 -0500

    Update teamc.py

--- teamc.py
+++ teamc.py
@@ -1,3 +1,5 @@
+import time
+
 roomArray = []
 itemArray = []
 for i in range(999):
@@ -7,15 +9,14 @@ roomArray[601] = "There is an antique box here with a jewel inside. There is a c
 roomArray[701] = "You found your brother!"
 roomArray[702] = "There is a chair to the south and a couch to the west. You hear loud screaming to the north."
 roomArray[703] = "There is a chair here with a hat sitting on it. You hear screaming to the north."
-roomArray[202] = "there is a table to your east and a tv to your south"
-roomArray[203] = "the tv is all static and doesnt work, there is a remote on the tv. looks like a hallway to your east"
-roomArray[302] = "there is a interesting painting on the table. there is opening in the wall to the south"
-roomArray[303] = "there is echoing coming down the hallway to your east and a tv to your west"
-roomArray[403] = "You have enetered the hallway. There are more rooms east. An Echo is getting slightly louder"
+roomArray[202] = "There is a table to your east and a TV to your south."
+roomArray[203] = "The TV is all static and doesnt work, there is a remote on the tv. It looks like there is a hallway to your east."
+roomArray[302] = "There is a interesting painting on the table. There is opening in the wall to the south."
+roomArray[303] = "There is echoing coming down the hallway to your east and a tv to your west."
+roomArray[403] = "You have enetered the hallway. There are more rooms east. An Echo is getting slightly louder."
 roomArray[503] = "You are close to the end of the hallway. There is a clear echo of someone. There is a coin in this room and a room to your east."
-roomArray[603] = "You are not in the hallway anymore. There is a chair to the east and a couch to the north."
+roomArray[603] = "You are not in the hallway anymore, you are in a study. There is a chair to the east and a couch to the north."
 roomArray[602] = "You are in a room with a couch and a scarf. There is an antique painting to the north."
-
 itemArray[601] = "Jewel"
 itemArray[703] = "Hat"
 itemArray[602] = "Scarf"
@@ -33,22 +34,36 @@ def doesRoomExist(roomNumber):
     except:
         print("You can't go there.")
         return False
-    
+
 def moveFunction(userInput, room):
-    if userInput == "n" and doesRoomExist() == True:
+    if userInput == "n" and doesRoomExist(room - 1) == True:
         room = room - 1
     else:
         print("you can not go here")
-    if userInput == "s" and doesRoomExist() == True:
+    if userInput == "s" and doesRoomExist(room + 1) == True:
         room = room + 1
     else:
         print("you can not go here")
-    if userInput == "e" and doesRoomExist() == True: 
+    if userInput == "e" and doesRoomExist(room + 100) == True: 
         room = room + 100
     else:
         print("you can not go here")
-    if userInput == "w" and doesRoomExist() == True:
+    if userInput == "w" and doesRoomExist(room - 100) == True:
         room = room - 100
     else:
         print("you can not go here")
     return room
+
+def main():
+    room = 202
+    print("Welcome to Mansion Mystery!")
+    time.sleep(1)
+    print("By Callie, Shayan, and Dalton.")
+    time.sleep(1)
+    print("You are in the living room of a mansion. You're brother has been captured by some angry ghosts, and it is your job to save him.")
+    time.sleep(2)
+    while True:
+        print(roomArray[room])
+        print("Please type: n, s, e, w, or quit.")
+        userinput = input()
+        room = moveFunction(userinput, room)
