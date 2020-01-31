47ea98b0f20946ebd452ac262077862d54dc3825
calliejh <57910979+calliejh@users.noreply.github.com>
2020-01-31 11:23:26 -0500

    Update teamc.py

--- teamc.py
+++ teamc.py
@@ -1,1 +1,15 @@
-
+roomArray = []
+itemArray = []
+for i in range(999):
+    roomArray.append(False)
+    itemArray.append(False)
+roomArray[601] = "There is an antique box here with a jewel inside. There is a couch to the south and you hear loud screaming to the east."
+roomArray[701] = "You found your brother!"
+roomArray[702] = "There is a chair to the south and a couch to the west. You hear loud screaming to the north."
+roomArray[703] = "There is a chair here with a hat sitting on it. You hear screaming to the north."
+itemArray[601] = "Jewel"
+itemArray[703] = "Hat"
+itemArray[602] = "Scarf"
+itemArray[503] = "Coin"
+itemArray[203] = "Remote"
+itemArray[302] = "Painting"
