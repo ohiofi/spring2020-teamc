496d86fd926d92fa0cc1e0d084c6cc2099932aa0
calliejh <57910979+calliejh@users.noreply.github.com>
2020-02-10 11:56:00 -0500

    Update teamc.py

--- teamc.py
+++ teamc.py
@@ -12,11 +12,11 @@ roomArray[701] = "You are in the north east corner of the study"
 roomArray[702] = "There is a chair to the south and a couch to the west. You see something shiny to the north."
 roomArray[703] = "There is a chair here with a hat sitting on it. There is a door to the west."
 roomArray[202] = "There is a table to your east and a TV to your south."
-roomArray[203] = "The TV is all static and doesn't work, there is a remote on the tv. It looks like there is a hallway to your east."
-roomArray[302] = "There is a interesting painting on the table. There is opening in the wall to the south."
+roomArray[203] = "The TV is all static and doesn't work. It looks like there is a hallway to your east."
+roomArray[302] = "There is a table here. There is opening in the wall to the south."
 roomArray[303] = "There is echoing coming down the hallway to your east and a tv to your west."
 roomArray[403] = "You have entered the hallway. There is something on the ground. There are more rooms east. An echo is getting slightly louder."
-roomArray[503] = "You are close to the end of the hallway. There is a window here and the echo is louder. There is a coin on the window sill and a room to your east."
+roomArray[503] = "You are close to the end of the hallway. There is a window here and the echo is louder. There is a room to your east."
 roomArray[603] = "You are not in the hallway anymore, you are in a study. There is a chair to the east, a couch to the north, and a door to the south."
 roomArray[602] = "There is a couch here with a couch and a scarf. There is an antique box to the north."
 roomArray[604] = "You are now in the kitchen. There is a table in front of you."
@@ -100,10 +100,6 @@ def startQuiz():
   askQuestion("Using letters, what does 2 divided by 1 equal?", answer = "two")
   askQuestion("Using letters, what does 4 plus 5 equal", answer = "nine")
 
-startQuiz()
-print("Your correct answers were: " + str(correctAnswers))
-print("Your wrong answers were: " + str(wrongAnswers))
-
 
 def main():
     room = 202
@@ -114,15 +110,18 @@ def main():
     print("You are in the living room of a mansion. You're brother has been captured by some angry ghosts, and it is your job to save him.")
     time.sleep(2)
     while True:
-      print(roomArray[room])
-      if not itemArray[room] == False:
-        print(itemArray[room])
-        print("Please type: n, s, e, w, take or quit.")
-      else:
-        print("Please type: n, s, e, w,  or quit.")
-      userInput = input()
-      room = moveFunction(userInput, room)
-      if userInput == "take":
-        print("You have taken this item: " + itemArray[room])
-      if userInput == "quit":
-        break
+        print(roomArray[room])
+        if not itemArray[room] == False:
+            print("Items here: " + itemArray[room])
+            print("Please type: n, s, e, w, take or quit.")
+            userInput = input()
+            room = moveFunction(userInput, room)
+        else:
+            print("Please type: n, s, e, w,  or quit.")
+            userInput = input()
+            room = moveFunction(userInput, room)
+        if userInput == "take":
+            print("You have taken this item: " + itemArray[room])
+            itemArray[room] = False
+        if userInput == "quit":
+            break
